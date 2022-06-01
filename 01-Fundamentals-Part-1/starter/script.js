3ef155e557c1c80477d1bdf23bbb2e2c038c1a5d
MinsuHan <trms7794han@gmail.com>
2022-06-01 15:13:34 +0900

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,8 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log(23);
+let firstName = "Matilda";
+console.log(firstName);
+
+let PI = 3.1415;
