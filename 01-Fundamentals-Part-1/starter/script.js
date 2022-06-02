83d18bb0e6eaece66a62b48976d98414ba74f16d
MinsuHan <trms7794han@gmail.com>
2022-06-02 21:39:34 +0900

    Operator Precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -40,7 +40,7 @@ const birthYear = 1991;
 
 var job = "programmer";
 job = "teacher";
-*/
+
 
 // Math operators
 const now = 2037;
@@ -68,3 +68,17 @@ console.log(ageJonas > ageSarah);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
