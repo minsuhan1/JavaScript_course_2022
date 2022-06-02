c50725bcb6adf7be7c7103aa6261113821ad5fd9
MinsuHan <trms7794han@gmail.com>
2022-06-02 21:59:58 +0900

    Taking Decisions: If / else Statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -93,7 +93,6 @@ const BMIJohn = massJohn / (heightJohn * heightJohn);
 const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn, markHigherBMI);
-*/
 
 const firstName = "Jonas";
 const job = "teacher";
@@ -116,3 +115,23 @@ lines`);
 console.log(`String
 multiple
 lines`);
+*/
+
+const age = 15;
+
+if (age >= 18) {
+  console.log("Sarah can start driving license");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 1998;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
