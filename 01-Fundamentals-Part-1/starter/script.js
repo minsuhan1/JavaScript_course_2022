ed48a876c4ce28d3138a2b83f9c0812439f47a77
MinsuHan <trms7794han@gmail.com>
2022-06-02 21:36:01 +0900

    Basic Operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -29,7 +29,7 @@ console.log(year);
 console.log(typeof year);
 
 console.log(typeof null); // null is a primitive value, not an object. (js bug)
-*/
+
 
 let age = 30;
 age = 31;
@@ -40,3 +40,31 @@ const birthYear = 1991;
 
 var job = "programmer";
 job = "teacher";
+*/
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
