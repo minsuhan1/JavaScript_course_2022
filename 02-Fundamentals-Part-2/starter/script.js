26b87ae7925960eac7a0c08f6c2fe77bd74353a3
MinsuHan <trms7794han@gmail.com>
2022-06-19 22:36:32 +0900

    Arrays, basic array operations(methods)

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -34,7 +34,6 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
-*/
 
 // function declaration (호이스팅 대상)
 function calcAge1(birthYear) {
@@ -63,8 +62,61 @@ function fruitProcessor(apples, oranges) {
   console.log(apples, oranges);
   const juice = `Juice with ${cutFruitPieces(
     apples
-  )} apples and ${cutFruitPieces(oranges)} oranges.`;
-  return juice;
-}
+    )} apples and ${cutFruitPieces(oranges)} oranges.`;
+    return juice;
+  }
+  
+  console.log(fruitProcessor(2, 3));
+  
+  const friends = ["Michael", "Steven", "Peter"];
+  console.log(friends);
+  
+  const years = new Array(1991, 1985, 2008, 2020);
+  
+  console.log(friends[0]);
+  console.log(friends[1]);
+  
+  console.log(friends.length);
+  console.log(friends[friends.length - 1]);
+  
+  friends[2] = "Jay"; // const 이지만 element는 변경가능
+  console.log(friends);
+  // friends = ["Bob", "Alice"];
+  
+  const jonas = ["Jnoas", "Schmedtmann", 2037 - 1991, "teacher", friends];
+  console.log(jonas);
+  console.log(jonas.length);
+  
+  // Exercise
+  const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+  };
+  
+  const years = [1990, 1967, 2002, 2010, 2018];
+*/
 
-console.log(fruitProcessor(2, 3));
+// Add elements
+const friends = ["Michael", "Steven", "Peter"];
+const newLength = friends.push("Jay");
+
+friends.unshift("John");
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+
+friends.push(23);
+console.log(friends.includes("Bob"));
+console.log(friends.includes(23));
+
+if (friends.includes("Steven")) {
+  console.log("yes");
+}
