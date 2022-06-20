41b6b8f4482002551ff818eb664d0045b456463a
MinsuHan <trms7794han@gmail.com>
2022-06-20 21:18:14 +0900

    Introduction to Objects, Dot vs Bracket notation

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -57,43 +57,41 @@ console.log(yearsUntilRetirement(1991, "Jonas"));
 function cutFruitPieces(fruit) {
   return fruit * 4;
 }
-
 function fruitProcessor(apples, oranges) {
   console.log(apples, oranges);
   const juice = `Juice with ${cutFruitPieces(
     apples
     )} apples and ${cutFruitPieces(oranges)} oranges.`;
     return juice;
-  }
-  
-  console.log(fruitProcessor(2, 3));
-  
-  const friends = ["Michael", "Steven", "Peter"];
-  console.log(friends);
-  
-  const years = new Array(1991, 1985, 2008, 2020);
-  
-  console.log(friends[0]);
-  console.log(friends[1]);
-  
-  console.log(friends.length);
-  console.log(friends[friends.length - 1]);
-  
-  friends[2] = "Jay"; // const 이지만 element는 변경가능
-  console.log(friends);
-  // friends = ["Bob", "Alice"];
-  
-  const jonas = ["Jnoas", "Schmedtmann", 2037 - 1991, "teacher", friends];
-  console.log(jonas);
-  console.log(jonas.length);
-  
-  // Exercise
-  const calcAge = function (birthYear) {
-    return 2037 - birthYear;
-  };
-  
-  const years = [1990, 1967, 2002, 2010, 2018];
-*/
+}
+
+console.log(fruitProcessor(2, 3));
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const years = new Array(1991, 1985, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[1]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay"; // const 이지만 element는 변경가능
+console.log(friends);
+// friends = ["Bob", "Alice"];
+
+const jonas = ["Jnoas", "Schmedtmann", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2010, 2018];
 
 // Add elements
 const friends = ["Michael", "Steven", "Peter"];
@@ -120,3 +118,44 @@ console.log(friends.includes(23));
 if (friends.includes("Steven")) {
   console.log("yes");
 }
+*/
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
+console.log(jonas);
+
+// DOT vs Bracket notation
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]);
+console.log(jonas["last" + nameKey]);
+
+// console.log(jonas."last" + nameKey);
+
+const interestedIn = prompt(
+  "What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends"
+);
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log("Wrong request");
+}
+
+jonas.location = "Portugal";
+jonas["twitter"] = "@jonasschmedtmann";
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
+);
