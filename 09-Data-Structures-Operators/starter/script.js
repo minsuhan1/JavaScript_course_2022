3cf7c9e505b56e7ccd31ba5ccc67c0c35bf4d137
MinsuHan <trms7794han@gmail.com>
2022-08-08 13:48:28 +0900

    Short Circuiting (&& and ||)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -52,6 +52,36 @@ const restaurant = {
   },
 };
 
+console.log('---- OR ----');
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spicach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spicach');
+
+/*
 // I) Distructuring
 // SPREAD, because on RIGHT side of =
 const arr = [1, 2, ...[3, 4]];
