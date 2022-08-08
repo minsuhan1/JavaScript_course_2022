64d61a2560ac252ac36740b7f62cc60d4f26556a
MinsuHan <trms7794han@gmail.com>
2022-08-08 13:52:50 +0900

    The Nullish Coalescing Operator (??)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -52,6 +52,15 @@ const restaurant = {
   },
 };
 
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+/*
 console.log('---- OR ----');
 // Use ANY data type, return ANY data type, short-circuiting
 console.log(3 || 'Jonas');
