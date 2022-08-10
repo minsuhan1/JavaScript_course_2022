fe0c56ccb11b6c5d503ee8f29c08e66fef0b4526
MinsuHan <trms7794han@gmail.com>
2022-08-10 11:34:28 +0900

    The call and apply Methods

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -83,7 +83,6 @@ const high5 = function () {
 document.body.addEventListener('click', high5);
 ['Jonas', 'Martha', 'Adam'].forEach(high5);
 
-*/
 
 const greet = function (greeting) {
   return function (name) {
@@ -103,3 +102,53 @@ const greetArr = greeting => name => {
 };
 
 greetArr('Hello')('Jonas');
+
+*/
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function() {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// book(23, 'Sarah Williams'); // regular function call -> does NOT work
+
+// Call method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+book.call(swiss, 583, 'Mary Cooper');
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+// ES6 이후로 apply method보다 스프레드 문법을 활용한 call method가 선호된다.
+book.call(swiss, ...flightData);
