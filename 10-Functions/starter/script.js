b1489e8572685211744712c81f5caa3542232c2a
MinsuHan <trms7794han@gmail.com>
2022-08-10 10:59:00 +0900

    Functions Returning Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -55,7 +55,6 @@ const newPassport = function (person) {
 newPassport(jonas);
 checkIn(flight, jonas);
 
-*/
 
 const oneWord = function (str) {
   return str.replace(/ /g, '').toLowerCase();
@@ -83,3 +82,24 @@ const high5 = function () {
 };
 document.body.addEventListener('click', high5);
 ['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+*/
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+// Challenge
+const greetArr = greeting => name => {
+  console.log(`${greeting} ${name}`);
+};
+
+greetArr('Hello')('Jonas');
