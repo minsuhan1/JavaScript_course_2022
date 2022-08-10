1449e729c959099a201f4ab32e91a3c5eac1950b
MinsuHan <trms7794han@gmail.com>
2022-08-10 10:53:02 +0900

    First-Class and Higher-Order Functions Functions Accepting Callback Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -25,7 +25,6 @@ createBooking('LH123');
 createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', undefined, 1000);
-*/
 
 const flight = 'LH234';
 const jonas = {
@@ -55,3 +54,32 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+
+*/
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('🤚');
+};
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
