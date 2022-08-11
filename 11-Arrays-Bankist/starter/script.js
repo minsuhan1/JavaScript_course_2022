c75b8d857235fb4581f65884e3b10da4111fd63a
MinsuHan <trms7794han@gmail.com>
2022-08-11 17:10:21 +0900

    The filter Method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -194,3 +194,14 @@ const movementDescriptions = movements.map((mov, i) => {
 console.log(movementDescriptions);
 
 */
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(function (mov, i, arr) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
