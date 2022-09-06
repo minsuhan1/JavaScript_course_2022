632f50a4153ec5175637f9136aa65601d5d06215
MinsuHan <trms7794han@gmail.com>
2022-09-06 17:16:14 +0900

    Lifecycle DOM Events

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -477,3 +477,17 @@ console.log(h1.parentElement.children);
 });
 
 */
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
