4d41781701594c8877dca365cd3a2a4232a34eaf
MinsuHan <trms7794han@gmail.com>
2023-02-19 22:24:58 +0900

    222. Another Class Example 223. Encapsulation: Protected Properties and Methods 224. Encapsulation: Private Class Fields and Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -332,3 +332,80 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'CS');
 jay.introduce();
 jay.calcAge();
+
+// Public fields
+// Private fields
+// Public methods
+// Private methods
+// (there is also static version)
+
+class Account {
+  // public fields (instances)
+  locale = navigator.language;
+
+  // private fields (instances)
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    // protected property
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // public methods
+
+  // Public interface
+  getMovements() {
+    return this.#movements;
+  }
+  deposit(val) {
+    this.#movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // private methods
+  // #approveLoan(val)
+  _approveLoan(val) {
+    return true;
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+console.log(acc1);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+console.log(acc1.getMovements());
+
+console.log(acc1);
+console.log(acc1.pin);
+
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100));
+
+Account.helper();
