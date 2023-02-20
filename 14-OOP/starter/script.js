b9c12cf0430938cb092cfe1b36ba8e92d164fa99
MinsuHan <trms7794han@gmail.com>
2023-02-20 14:18:28 +0900

    225. Chaining Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -366,16 +366,19 @@ class Account {
   }
   deposit(val) {
     this.#movements.push(val);
+    return this;
   }
 
   withdraw(val) {
     this.deposit(-val);
+    return this;
   }
 
   requestLoan(val) {
     if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
+      return this;
     }
   }
 
@@ -403,9 +406,12 @@ console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1.pin);
+Account.helper();
 
 // console.log(acc1.#movements);
 // console.log(acc1.#pin);
 // console.log(acc1.#approveLoan(100));
 
-Account.helper();
+// Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
