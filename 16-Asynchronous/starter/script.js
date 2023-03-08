365ce6228214bd312ea9209a89caf96139788e72
MinsuHan <trms7794han@gmail.com>
2023-03-08 10:30:32 +0900

    257. Asynchronous Behind the Scenes: The Event Loop / 258. The Event Loop in Practice

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -198,3 +198,19 @@ btn.addEventListener('click', function () {
 });
 
 getCountryData('australia');
+
+console.log('Test start');
+
+setTimeout(() => console.log('0 sec timer'), 0);
+
+Promise.resolve('Resolved promise 1').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end');
