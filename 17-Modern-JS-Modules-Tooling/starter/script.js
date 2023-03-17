34e06949262e312f96c180d57058ca7c964a4ce2
MinsuHan <trms7794han@gmail.com>
2023-03-17 09:55:38 +0900

    273. Top-Level await (ES2022)

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -16,3 +16,24 @@ add('pizza', 2);
 add('bread', 5);
 add('apples', 4);
 console.log(cart);
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+// top-level await
+const lastPost = await getLastPost();
+console.log(lastPost);
